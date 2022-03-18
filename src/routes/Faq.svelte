652305c6129cbbe87d5000b923095ac183f05ab8
Yuya Ogawa <yuya.ogw@gmail.com>
2022-03-18 21:33:02 +0900

    Fix FAQ page

--- src/routes/Faq.svelte
+++ src/routes/Faq.svelte
@@ -1,15 +1,25 @@
 <main>
   <h2>FAQ</h2>
   <br />
+
   <h3>How to play?</h3>
   <ul>
     <li>1. Select Yes/No and provide an invoice for your premium</li>
     <li>2. Pay to holdinvoice and wait the result</li>
   </ul>
   <p>That is all! Once Oracle signs the outcome we settle/cancel the held invoice.</p>
+  <hr>
+
+  <h3>Overview</h3>
+  <p><img src='https://s3-ap-northeast-1.amazonaws.com/spotlight-s3-001/article/20220317_044111_mceu_89623369121647492071491.png' alt='diagram'></p>
+  <h3>API</h3>
+  <p><a href="http://testnet.dlc.soy:3000/api-documentation/" target="_blank">DLC-LN</a></p>
+  <p><a href="http://testnet.dlc.soy:4000/api-documentation/" target="_blank">Oracle Server</a></p>
+  <hr>
 
   <h3>Can Maker cheat?</h3>
   <p>Maker still can cheat the deal, however when Taker gets a preimage that is not supposed to be revealed/decrypted, this is a Proof of Fraud and they can publish to anywhere.</p>
+  <hr>
 
   <h3>How does this work?</h3>
   <p><img src='https://s3-ap-northeast-1.amazonaws.com/spotlight-s3-001/article/20220309_110438_mceu_35637501011646823877750.png' alt='diagram'></p>
