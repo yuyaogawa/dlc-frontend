93aefff38bf1de05fb450632416350a8953edbd5
Yuya Ogawa <yuya.ogw@gmail.com>
2023-11-05 20:12:35 +0900

    Fix style

--- src/routes/Dlc.svelte
+++ src/routes/Dlc.svelte
@@ -201,7 +201,7 @@
       interval: '1',
       timezone: 'Etc/UTC',
       theme: 'dark',
-      style: '2',
+      style: '1',
       locale: 'en',
       toolbar_bg: '#f1f3f6',
       enable_publishing: false,
